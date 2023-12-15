99cb043dd0c98993faa8a270531ed5b6f31319dd
Juliette Rousselet <juliette.roussleet@etu.univ-orleans.fr>
2023-12-15 10:47:02 +0100

    petit nettoyage

--- Server.java
+++ Server.java
@@ -11,11 +11,11 @@ public class Server implements Runnable {
     
     private ServerSocket serverSocket;
     private BufferedReader reader;
-    private Map<ClientHandler, List<ClientHandler>> abonnes;
+    private Map<ClientHandler, List<ClientHandler>> clients;
 
     public Server() throws Exception {
         this.serverSocket = new ServerSocket(4444);
-        this.abonnes = new HashMap<>();
+        this.clients = new HashMap<>();
     }
 
     @Override
@@ -24,7 +24,7 @@ public class Server implements Runnable {
             while(true) {
                 // System.out.println("Waiting for client...");
                 ClientHandler clientHandler = new ClientHandler(serverSocket.accept(),this);
-                this.abonnes.put(clientHandler, new ArrayList<>());
+                this.clients.put(clientHandler, new ArrayList<>());
                 Thread clientThread = new Thread(clientHandler);
                 clientThread.start();
             }
@@ -34,37 +34,12 @@ public class Server implements Runnable {
         }
     }
 
-    public List<String> getUtilisateurs() {
-        List<String> utilisateurs = new ArrayList<>();
-        for (ClientHandler client : this.abonnes.keySet()) {
-            utilisateurs.add(client.getName());
-        }
-        return utilisateurs;
-    }
+    public void handleMessage(String message, ClientHandler clientHandler) {
 
-    public List<ClientHandler> getAbonnes(ClientHandler clientHandler) {
-        List<ClientHandler> abonnes = new ArrayList<>();
-        for (ClientHandler client : this.abonnes.get(clientHandler)) {
-            abonnes.add(client);
-        }
-        return abonnes;
-    }
-
-    public Map<ClientHandler, List<ClientHandler>> getAbonnes() {
-        return this.abonnes;
-    }
-
-    public List<ClientHandler> getAbonnes(String nomClient) {
-        for (ClientHandler client : this.abonnes.keySet()) {
-            if (client.getName().equals(nomClient)) {
-                return this.abonnes.get(client);
-            }
-        }
-        return null;
     }
 
     public void broadcast(String message, ClientHandler clientHandler){
-        for (ClientHandler client : this.abonnes.keySet()) {
+        for (ClientHandler client : this.clients.keySet()) {
             if (!client.getName().equals(clientHandler.getName())) {
                 client.sendMessage(clientHandler.getName() + ">" + message);
             }
@@ -72,7 +47,7 @@ public class Server implements Runnable {
     }
 
     public void sendTo(String nomClient, String message, ClientHandler clientHandler) {
-        for (ClientHandler client : this.abonnes.keySet()) {
+        for (ClientHandler client : this.clients.keySet()) {
             if (client.getName().equals(nomClient)) {
                 client.sendMessage(clientHandler.getName() + "->You>" + message);
             }
@@ -80,41 +55,78 @@ public class Server implements Runnable {
     }
 
     public void sendToAbonnes(String message, ClientHandler clientHandler) {
-        for (ClientHandler client : this.abonnes.get(clientHandler)) {
+        for (ClientHandler client : this.clients.get(clientHandler)) {
             client.sendMessage(clientHandler.getName() + ">" + message);
         }
     }
 
+    // Getters et setters
+
+    public List<String> getUtilisateurs() {
+        List<String> utilisateurs = new ArrayList<>();
+        for (ClientHandler client : this.clients.keySet()) {
+            utilisateurs.add(client.getName());
+        }
+        return utilisateurs;
+    }
+
+    public List<ClientHandler> getAbonnes(ClientHandler clientHandler) {
+        List<ClientHandler> abonnes = new ArrayList<>();
+        for (ClientHandler client : this.clients.get(clientHandler)) {
+            abonnes.add(client);
+        }
+        return abonnes;
+    }
+
+    public Map<ClientHandler, List<ClientHandler>> getAbonnes() {
+        return this.clients;
+    }
+
+    public List<ClientHandler> getAbonnes(String nomClient) {
+        for (ClientHandler client : this.clients.keySet()) {
+            if (client.getName().equals(nomClient)) {
+                return this.clients.get(client);
+            }
+        }
+        return null;
+    }
+
     public void addAbonne(String nomClient, ClientHandler clientHandler) {
         if (!nomClient.equals(clientHandler.getName())) {
-            for (ClientHandler client : this.abonnes.keySet()) {
+            for (ClientHandler client : this.clients.keySet()) {
                 if (client.getName().equals(nomClient)) {
-                    this.abonnes.get(client).add(clientHandler);
+                    this.clients.get(client).add(clientHandler);
                 }
             }
         }
     }
 
     public void removeAbonne(String nomClient, ClientHandler clientHandler) {
-        for (ClientHandler client : this.abonnes.keySet()) {
+        for (ClientHandler client : this.clients.keySet()) {
             if (client.getName().equals(nomClient)) {
-                this.abonnes.get(client).remove(clientHandler);
+                this.clients.get(client).remove(clientHandler);
             }
         }
     }
 
     public void removeClient(ClientHandler clientHandler) {
-        this.abonnes.remove(clientHandler);
+        this.clients.remove(clientHandler);
+    }
+
+    public int getNombreConnectes(){
+        return this.clients.keySet().size();
     }
 
     public boolean nameAlreadyUsed(String name) {
-        for (ClientHandler client : this.abonnes.keySet()) {
+        for (ClientHandler client : this.clients.keySet()) {
             if (client.getName().equals(name)) {
                 return true;
             }
         }
         return false;
     }
+
+    // Autres
 
     private void closeServer() {
         System.out.println("Closing server...");
@@ -123,10 +135,6 @@ public class Server implements Runnable {
         } catch(Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public int getNombreConnectes(){
-        return this.abonnes.keySet().size();
     }
 
     public static void main(String[] args) throws Exception{
